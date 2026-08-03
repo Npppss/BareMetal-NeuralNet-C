46fff798e04025e137bf7fe67b06417927a94c7e
Npppss <novandrae7980@gmail.com>
2026-08-03 16:20:08 +0700

    add matrix_math.exe Compiler

--- layer.c
+++ layer.c
@@ -154,3 +154,13 @@ Matrix* forward_pass(DenseLayer *layer, Matrix *input){
 
     return layer->A;
 }
+
+// Fungsi Pembebas Memori Layer
+void free_dense_layer(DenseLayer *layer) {
+    if (layer == NULL) return;
+    free_matrix(layer->weights);
+    free_matrix(layer->biases);
+    if (layer->Z != NULL) free_matrix(layer->Z);
+    if (layer->A != NULL) free_matrix(layer->A);
+    free(layer);
+}
