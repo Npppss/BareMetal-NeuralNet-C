ecff51499fc2e993c208c5033db39b00e1f7ae8b
Npppss <novandrae7980@gmail.com>
2026-08-03 18:17:18 +0700

    add block code create_matrix

--- ann.c
+++ ann.c
@@ -2,3 +2,19 @@
 #include <stdlib.h>
 #include <math.h> // WAJIB untuk fungsi exp()
 #include <time.h> // Dibutuhkan untuk membuat seed random (acak)
+
+typedef struct {
+    int rows;
+    int cols;
+    double **data;
+} Matrix;
+
+Matrix* create_matrix(int rows, int cols){
+    Matrix *m = (Matrix*)malloc(sizeof(Matrix));
+    m->rows = rows; m->cols = cols;
+    m->data = (double**)malloc(rows * sizeof(double*));
+    for (int i = 0; i < rows; i++){
+        m->data[i] = (double*)calloc(cols, sizeof(double));
+    }
+    return m;
+}
